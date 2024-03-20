8a2d282d6bf46a4f81fd4baa0ebdd93552410a81
bondtxus <bondtxus@outlook.com>
2024-03-20 15:18:10 +0900

    No4

---
+++ No4/StandardExceptions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class CanGoWrong {
+  public:
+    CanGoWrong() {
+      char *pMemory = new char[999999999999999999999999999999999];
+      delete [] pMemory;
+    }
+};
+
+int main() {
+
+  try {
+    CanGoWrong wrong;
+  }
+  catch(bad_alloc &e) {
+    cout << "Caught exception: " << e.what() << endl;
+  }
+  cout << "Still runnning" << endl;
+
+  return 0;
+}
